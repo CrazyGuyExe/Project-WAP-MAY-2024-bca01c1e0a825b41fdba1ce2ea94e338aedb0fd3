1ff996955b1854548d3c07ff0748cd05a6a79879
Your Name <you@example.com>
2024-06-08 09:28:51 +0200

    Lišta pridaná+ Matěj fixni+ikony

--- client/src/pages/MainPage/MainPage.jsx
+++ client/src/pages/MainPage/MainPage.jsx
@@ -22,62 +22,72 @@ export default function MainPage() {
         else return body.substring(0, length) + '...'
     }
     return (
-        //gray box
-        <div className="mx-auto mt-3 max-w-4xl rounded-md bg-gray-50 p-8">
-            <h1 className="mb-7 border-b border-gray-300 pb-4 text-center text-4xl font-extrabold text-gray-800">
-                NÁZEV
-            </h1>
-            <div className="mb-8 flex justify-center space-x-4">
-                <Link
-                    to="/createarticle"
-                    className="relative mr-1 inline-block rounded-md border border-transparent bg-slate-50 px-3 py-1 shadow-sm transition-colors duration-300 hover:bg-gray-600 hover:text-white"
-                >
-                    Create article
-                </Link>
-                <Link
-                    to="/articles"
-                    className="relative mr-2 inline-block rounded-md border border-transparent bg-slate-50 px-3 py-1 shadow-sm transition-colors duration-300 hover:bg-gray-600 hover:text-white"
-                >
-                    Articles
-                </Link>
-                <Link
-                    to="/search"
-                    className="relative mr-2 inline-block rounded-md border border-transparent bg-slate-50 px-3 py-1 shadow-sm transition-colors duration-300 hover:bg-gray-600 hover:text-white"
-                >
-                    Search Articles
-                </Link>
-            </div>
+        <div className="">
+            <header className="bg-gray-50 shadow-sm">
+                <div className="container mx-auto flex items-center justify-between p-4">
+                    <nav className="flex space-x-4">
+                    <h2 className="">
+                    NÁZEV
+                </h2>
+                        <Link
+                            to="/createarticle"
+                            className="relative mr-1 inline-block rounded-md border border-transparent bg-gray-200 px-3 py-1 shadow-sm transition-colors duration-300 hover:bg-gray-600 hover:text-white"
+                        >
+                            Create Article
+                        </Link>
+                        <Link
+                            to="/articles"
+                            className="relative mr-1 inline-block rounded-md border border-transparent bg-gray-200 px-3 py-1 shadow-sm transition-colors duration-300 hover:bg-gray-600 hover:text-white"
+                        >
+                            Articles
+                        </Link>
+                        <Link
+                            to="/search"
+                            className="relative mr-1 inline-block rounded-md border border-transparent bg-gray-200 px-3 py-1 shadow-sm transition-colors duration-300 hover:bg-gray-600 hover:text-white"
+                        >
+                            Search Articles
+                        </Link>
+                    </nav>
+                </div>
+            </header>
 
-            {randomArticle && (
-                <div className="p rounded-lg bg-gray-100 p-6 shadow-md">
-                    <h2 className="mb-4 text-2xl font-semibold">
-                        Recommendation:
-                    </h2>
-                    <h3 className="mb-2 text-xl font-medium">
-                        <ArticleLink {...randomArticle} />
-                    </h3>
-                    <p className="text-gray-700">
-                        {shorterText(randomArticle.body)}
-                    </p>
-                </div>
-            )}
-              <footer className="border-t border-gray-300 mt-16 pt-4 font-medium">
-                <div className="flex justify-between text-gray-600">
-                    <div>
-                        <p className="text-lg font-semibold">Our Free Encyclopedia</p>
+            <div className="mx-auto mt-3 max-w-4xl rounded-md bg-gray-50 p-8">
+             
+               
+
+                {randomArticle && (
+                    <div className="p rounded-lg bg-gray-100 p-6 shadow-md">
+                        <h2 className="mb-4 text-2xl font-semibold">
+                            Recommendation:
+                        </h2>
+                        <h3 className="mb-2 text-xl font-medium">
+                            <ArticleLink {...randomArticle} />
+                        </h3>
+                        <p className="text-gray-700">
+                            {shorterText(randomArticle.body)}
+                        </p>
                     </div>
-                    <div className="font-medium">
+                )}
+                <footer className="mt-16 border-t border-gray-300 pt-4 font-medium">
+                    <div className="flex justify-between text-gray-600">
                         <div>
-                            <p>Matěj Landa</p>
-                            <p>Marek Kubelka</p>
+                            <p className="text-lg font-semibold">
+                                Our Free Encyclopedia
+                            </p>
                         </div>
-                        <div>
-                            <p>Dominick Correia</p>
-                            <p>Vojta Vlček</p>
+                        <div className="font-medium">
+                            <div>
+                                <p>Matěj Landa</p>
+                                <p>Marek Kubelka</p>
+                            </div>
+                            <div>
+                                <p>Dominick Correia</p>
+                                <p>Vojta Vlček</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </footer>
+                </footer>
+            </div>
         </div>
     )
 }
